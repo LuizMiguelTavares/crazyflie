9cc9deee03cdd013f3b0c5ad8756c791d76c60be
Luiz Miguel Tavares <luizmiguel.pessotti@gmail.com>
2025-01-31 13:11:37 -0300

    Added angle rate and organized the LOG and twist message

--- crazyflie_server/scripts/crazyflie_server.py
+++ crazyflie_server/scripts/crazyflie_server.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 import rospy
 
-from geometry_msgs.msg import  Twist, Vector3
-
+from geometry_msgs.msg import  Twist, Vector3Stamped
 import cflib
 from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.log import LogConfig
@@ -19,17 +18,30 @@ class CrazyflieServerNode:
         self._cf = Crazyflie()
 
         self.vel_flag = False
-        self.angles_flag = False
         self.thrust_flag = False
         self.ang_flag = False
         self.acc_flag = False
+        self.gyro_raw_flag = False
+        self.gyro_flag = False
 
         # Crazyflie connection callbacks
         self._cf.connected.add_callback(self._connected)
         self._cf.disconnected.add_callback(self._disconnected)
         self._cf.connection_failed.add_callback(self._connection_failed)
-        self._cf.connection_lost.add_callback(self._connection_lost)        
-
+        self._cf.connection_lost.add_callback(self._connection_lost) 
+        
+        # Choose between ang or body rate
+        self.use_body_rate = rospy.get_param('~use_body_rate', False)
+        
+        # LOG params
+        # self.crazyflie_LOG = rospy.get_param('~crazyflie_LOG', True)
+        self.vel_LOG = rospy.get_param('~vel_LOG', True)
+        self.ang_LOG = rospy.get_param('~ang_LOG', True)
+        self.thrust_LOG = rospy.get_param('~thrust_LOG', True)
+        self.acc_LOG = rospy.get_param('~acc_LOG', True)
+        self.gyro_raw_LOG = rospy.get_param('~gyro_raw_LOG', True)
+        self.gyro_LOG = rospy.get_param('~gyro_LOG', True)
+              
         ID = rospy.get_param('~ID', None)
         
         if ID == 1:
@@ -95,20 +107,34 @@ class CrazyflieServerNode:
         # self._cf.param.set_value("pid_attitude.pitch_ki", 0)
         # self._cf.param.set_value("pid_attitude.roll_ki", 0)
         # self._cf.param.set_value("pid_attitude.yaw_ki", 0)
-        self._cf.param.set_value("flightmode.stabModeRoll", 0)
-        self._cf.param.set_value("flightmode.stabModePitch", 0)
-        self._cf.param.set_value("flightmode.stabModeYaw", 0)
+        
+        if self.use_body_rate:
+            self._cf.param.set_value("flightmode.stabModeRoll", 0)
+            self._cf.param.set_value("flightmode.stabModePitch", 0)
+            self._cf.param.set_value("flightmode.stabModeYaw", 0)
+        else:
+            self._cf.param.set_value("flightmode.stabModeRoll", 1)
+            self._cf.param.set_value("flightmode.stabModePitch", 1)
+            self._cf.param.set_value("flightmode.stabModeYaw", 1)
 
         ################### CRAZYFLIE TOPICS #######################
         self.pose_subscriber = rospy.Subscriber(f"cmd_vel",
-                                                Twist, 
+                                                Twist,
                                                 self.publish_twist)
         
         # Publishers for different sets of data
-        self.pub_log = rospy.Publisher('crazyflieLog', CrazyflieLog, queue_size=10)
-        self.pub_only_vel = rospy.Publisher('crazyflieVel', Vector3, queue_size=10)
-        self.pub_only_ang = rospy.Publisher('crazyflieAng', Twist, queue_size=10)
-        self.pub_only_acc = rospy.Publisher('crazyflieAcc', Vector3, queue_size=10)
+        if self.vel_LOG:
+            self.pub_vel = rospy.Publisher('crazyflieVel', Vector3Stamped, queue_size=10)
+        if self.ang_LOG:
+            self.pub_ang = rospy.Publisher('crazyflieAng', Vector3Stamped, queue_size=10)
+        if self.thrust_LOG:
+            self.pub_thrust = rospy.Publisher('crazyflieThrust', Vector3Stamped, queue_size=10)
+        if self.acc_LOG:
+            self.pub_acc = rospy.Publisher('crazyflieAcc', Vector3Stamped, queue_size=10)
+        if self.gyro_raw_LOG:
+            self.pub_raw_gyro = rospy.Publisher('crazyflieRawAngRate', Vector3Stamped, queue_size=10)
+        if self.gyro_LOG:
+            self.pub_gyro = rospy.Publisher('crazyflieAngRate', Vector3Stamped, queue_size=10)
 
         self.timer = rospy.Timer(rospy.Duration(1.0/60.0), self._send_log_data)
 
@@ -116,54 +142,96 @@ class CrazyflieServerNode:
         """ This callback is called form the Crazyflie API when a Crazyflie
         has been connected and the TOCs have been downloaded."""
         rospy.loginfo(f'Crazyflie {link_uri} connected!')
+        rospy.sleep(1)
 
-        self._lg_vel = LogConfig(name='velocity in the drone frame', period_in_ms=1000/60)
-        self._lg_vel.add_variable('stateEstimate.vx', 'float')
-        self._lg_vel.add_variable('stateEstimate.vy', 'float')
-        self._lg_vel.add_variable('stateEstimate.vz', 'float')
-        self._lg_vel.add_variable('stateEstimate.z', 'float')
-        
-        self._lg_ang = LogConfig(name='Angle of the drone', period_in_ms=1000/60)
-        self._lg_ang.add_variable('stateEstimate.pitch', 'float')
-        self._lg_ang.add_variable('stateEstimate.roll', 'float')
-        self._lg_ang.add_variable('stateEstimate.yaw', 'float')
+        if self.thrust_LOG:
+            self._lg_thrust = LogConfig(name='thrust', period_in_ms=1000/60)
+            self._lg_thrust.add_variable('stabilizer.thrust', 'float')
+            
+            try:
+                rospy.loginfo("Adding thrust log...")
+                self._cf.log.add_config(self._lg_thrust)
+                self._lg_thrust.data_received_cb.add_callback(self._thrust_data)
+                self._lg_thrust.error_cb.add_callback(self._vel_log_error)
+                self._lg_thrust.start()
+            except KeyError as e:
+                print('Could not start log configuration,'
+                    '{} not found in TOC'.format(str(e)))
+            except AttributeError:
+                print('Could not add Stabilizer log config, bad configuration.')
+                
+        if self.ang_LOG:
+            self._lg_ang = LogConfig(name='Angle of the drone', period_in_ms=1000/60)
+            self._lg_ang.add_variable('stateEstimate.pitch', 'float')
+            self._lg_ang.add_variable('stateEstimate.roll', 'float')
+            self._lg_ang.add_variable('stateEstimate.yaw', 'float')
 
-        self._lg_thrust = LogConfig(name='thrust', period_in_ms=1000/60)
-        self._lg_thrust.add_variable('stabilizer.thrust', 'float')
+            try:
+                rospy.loginfo("Adding angle log...")
+                self._cf.log.add_config(self._lg_ang)
+                self._lg_ang.data_received_cb.add_callback(self._ang_log_data)
+                self._lg_ang.error_cb.add_callback(self._vel_log_error)
+                self._lg_ang.start()
+            except KeyError as e:
+                print('Could not start log configuration,'
+                    '{} not found in TOC'.format(str(e)))
+            except AttributeError:
+                print('Could not add Stabilizer log config, bad configuration.')
         
-        self._lg_acc = LogConfig(name='Acceleration in the drone frame', period_in_ms=1000/60)
-        self._lg_acc.add_variable('acc.x', 'float')
-        self._lg_acc.add_variable('acc.y', 'float')
-        self._lg_acc.add_variable('acc.z', 'float')
+        if self.acc_LOG:
+            self._lg_acc = LogConfig(name='Acceleration in the drone frame', period_in_ms=1000/60)
+            self._lg_acc.add_variable('acc.x', 'float')
+            self._lg_acc.add_variable('acc.y', 'float')
+            self._lg_acc.add_variable('acc.z', 'float')
+
+            try:
+                rospy.loginfo("Adding acceleration log...")
+                self._cf.log.add_config(self._lg_acc)
+                self._lg_acc.data_received_cb.add_callback(self._acc_log_data)
+                self._lg_acc.error_cb.add_callback(self._vel_log_error)
+                self._lg_acc.start()
+            except KeyError as e:
+                print('Could not start log configuration,'
+                    '{} not found in TOC'.format(str(e)))
+            except AttributeError:
+                print('Could not add Stabilizer log config, bad configuration.')
+                
+        if self.gyro_raw_LOG:
+            self._lg_gyro_raw = LogConfig(name='Gyro Raw in the drone frame', period_in_ms=1000/60)
+            self._lg_gyro_raw.add_variable('gyro.xRaw', 'int16_t')
+            self._lg_gyro_raw.add_variable('gyro.yRaw', 'int16_t')
+            self._lg_gyro_raw.add_variable('gyro.zRaw', 'int16_t')
+
+            try:
+                rospy.loginfo("Adding raw gyro log...")
+                self._cf.log.add_config(self._lg_gyro_raw)
+                self._lg_gyro_raw.data_received_cb.add_callback(self._gyro_raw_log_data)
+                self._lg_gyro_raw.error_cb.add_callback(self._gyro_raw_log_error)
+                self._lg_gyro_raw.start()
+            except KeyError as e:
+                print('Could not start log configuration,'
+                    '{} not found in TOC'.format(str(e)))
+            except AttributeError:
+                print('Could not add Stabilizer log config, bad configuration.')
+            
         
-        try:
-            self._cf.log.add_config(self._lg_vel)
-            self._cf.log.add_config(self._lg_thrust)
-            self._cf.log.add_config(self._lg_ang)
-            self._cf.log.add_config(self._lg_acc)
-            
-            # This callback will receive the data
-            self._lg_vel.data_received_cb.add_callback(self._vel_log_data)
-            self._lg_thrust.data_received_cb.add_callback(self._thrust_data)
-            self._lg_ang.data_received_cb.add_callback(self._ang_log_data)
-            self._lg_acc.data_received_cb.add_callback(self._acc_log_data)
-            
-            # This callback will be called on errors
-            self._lg_vel.error_cb.add_callback(self._vel_log_error)
-            self._lg_thrust.error_cb.add_callback(self._vel_log_error)
-            self._lg_ang.error_cb.add_callback(self._vel_log_error)
-            self._lg_acc.error_cb.add_callback(self._vel_log_error)
-            
-            # Start the logging
-            self._lg_vel.start()
-            self._lg_thrust.start()
-            self._lg_ang.start()
-            self._lg_acc.start()
-        except KeyError as e:
-            print('Could not start log configuration,'
-                  '{} not found in TOC'.format(str(e)))
-        except AttributeError:
-            print('Could not add Stabilizer log config, bad configuration.')
+        if self.gyro_LOG:
+            self._lg_gyro = LogConfig(name='Gyro in the drone frame', period_in_ms=1000/60)
+            self._lg_gyro.add_variable('gyro.x', 'float')
+            self._lg_gyro.add_variable('gyro.y', 'float')
+            self._lg_gyro.add_variable('gyro.z', 'float')
+
+            try:
+                rospy.loginfo("Adding filtered gyro log...")
+                self._cf.log.add_config(self._lg_gyro)  # Suspect
+                self._lg_gyro.data_received_cb.add_callback(self._gyro_log_data)
+                self._lg_gyro.error_cb.add_callback(self._gyro_log_error)
+                self._lg_gyro.start()
+            except KeyError as e:
+                print('Could not start log configuration,'
+                    '{} not found in TOC'.format(str(e)))
+            except AttributeError:
+                print('Could not add Stabilizer log config, bad configuration.')
 
     def _connection_failed(self, link_uri, msg):
         """Callback when connection initial connection fails (i.e no Crazyflie
@@ -194,6 +262,14 @@ class CrazyflieServerNode:
     def _acc_log_error(self, logconf, msg):
         """Callback from the log API when an error occurs"""
         print('Error when logging %s: %s' % (logconf.name, msg))
+    
+    def _gyro_raw_log_error(self, logconf, msg):
+        """Callback from the log API when an error occurs"""
+        print('Error when logging %s: %s' % (logconf.name, msg))
+        
+    def _gyro_log_error(self, logconf, msg):
+        """Callback from the log API when an error occurs"""
+        print('Error when logging %s: %s' % (logconf.name, msg))
 
     def _vel_log_data(self, timestamp, data, logconf):
         """Callback from a the log API when data arrives"""
@@ -204,11 +280,26 @@ class CrazyflieServerNode:
         self.vx = data.get('stateEstimate.vx', 0)
         self.vy = data.get('stateEstimate.vy', 0)
         self.vz = data.get('stateEstimate.vz', 0)
-        self.z = data.get('stateEstimate.z', 0)  
-        # self.pitch = data.get('stateEstimate.pitch', 0)
-        # self.roll = data.get('stateEstimate.roll', 0)
-        # self.yaw = data.get('stateEstimate.yaw', 0)
-
+        self.z = data.get('stateEstimate.z', 0)
+    
+    def _gyro_raw_log_data(self, timestamp, data, logconf):
+        """Callback from a the log API when data arrives"""
+        if not self.gyro_raw_flag:
+            rospy.loginfo("Raw Gyro log started")
+            self.gyro_raw_flag = True
+        self.gyro_x_raw = data.get('gyro.xRaw', 0)
+        self.gyro_y_raw = data.get('gyro.yRaw', 0)
+        self.gyro_z_raw = data.get('gyro.zRaw', 0)
+        
+    def _gyro_log_data(self, timestamp, data, logconf):
+        """Callback from a the log API when data arrives"""
+        if not self.gyro_flag:
+            rospy.loginfo("Gyro log started")
+            self.gyro_flag = True
+        self.gyro_x = data.get('gyro.x', 0)
+        self.gyro_y = data.get('gyro.y', 0)
+        self.gyro_z = data.get('gyro.z', 0)
+        
     def _thrust_data(self, timestamp, data, logconf):
         """Callback from a the log API when data arrives"""
         if not self.thrust_flag:
@@ -221,8 +312,9 @@ class CrazyflieServerNode:
         if not self.ang_flag:
             rospy.loginfo("Angle log started")
             self.ang_flag = True
-        self.pitch = data.get('stateEstimate.pitch', 0)
+            
         self.roll = data.get('stateEstimate.roll', 0)
+        self.pitch = data.get('stateEstimate.pitch', 0)
         self.yaw = data.get('stateEstimate.yaw', 0)
     
     def _acc_log_data(self, timestamp, data, logconf):
@@ -235,48 +327,99 @@ class CrazyflieServerNode:
         self.acc_z = data.get('acc.z', 0)
 
     def _send_log_data(self, event):
-        if self.vel_flag and self.thrust_flag and self.ang_flag and self.acc_flag:
-            log_msg = CrazyflieLog()
-            vel_msg = Vector3()
-            ang_msg = Twist()
-            acc_msg = Vector3()
-
-            timestamp = rospy.Time.now().to_sec()
-            log_msg.timestamp = timestamp
-
-            log_msg.vx = self.vx
-            log_msg.vy = self.vy    
-            log_msg.vz = self.vz
-            log_msg.z = self.z
-            log_msg.pitch = self.pitch
-            log_msg.roll = self.roll
-            log_msg.thrust = self.thrust
-            log_msg.yaw = self.yaw
         
-            vel_msg.x = self.vx
-            vel_msg.y = self.vy
-            vel_msg.z = self.vz
-
-            ang_msg.linear.x = self.pitch
-            ang_msg.linear.y = self.roll
-            ang_msg.linear.z = self.thrust
-            ang_msg.angular.z = self.yaw
+        if self.ang_LOG and self.ang_flag:
+            ang_msg = Vector3Stamped()
+            ang_msg.header.stamp = rospy.Time.now()
             
-            acc_msg.x = self.acc_x
-            acc_msg.y = self.acc_y
-            acc_msg.z = self.acc_z
+            # deg to rad
+            roll = self.roll * 0.0174533
+            pitch = self.pitch * 0.0174533
+            yaw = self.yaw * 0.0174533
 
-            self.pub_log.publish(log_msg)
-            self.pub_only_vel.publish(vel_msg)
-            self.pub_only_ang.publish(ang_msg)
-            self.pub_only_acc.publish(acc_msg)
+            ang_msg.vector.x = roll
+            ang_msg.vector.y = -pitch
+            ang_msg.vector.z = yaw
+            self.pub_ang.publish(ang_msg)
+            
+        if self.vel_LOG and self.vel_flag:
+            vel_msg = Vector3Stamped()
+            vel_msg.header.stamp = rospy.Time.now()
+            vel_msg.vector.x = self.vx
+            vel_msg.vector.y = self.vy
+            vel_msg.vector.z = self.vz
+            self.pub_vel.publish(vel_msg)
+            
+        if self.thrust_LOG and self.thrust_flag:
+            thrust_msg = Vector3Stamped()
+            thrust_msg.header.stamp = rospy.Time.now()
+            thrust_msg.vector.z = self.thrust
+            self.pub_thrust.publish(thrust_msg)
+        
+        if self.acc_LOG and self.acc_flag:
+            acc_msg = Vector3Stamped()
+            acc_msg.header.stamp = rospy.Time.now()
+            gravity = 9.81
+            acc_msg.vector.x = -self.acc_x * gravity
+            acc_msg.vector.y = -self.acc_y * gravity
+            acc_msg.vector.z = -self.acc_z * gravity
+            self.pub_acc.publish(acc_msg)
+        
+        if self.gyro_raw_LOG and self.gyro_raw_flag:
+            gyro_raw_msg = Vector3Stamped()
+            gyro_raw_msg.header.stamp = rospy.Time.now()
+            
+            # Int to rad/s
+            gyro_x_raw = self.gyro_x_raw * 0.001065
+            gyro_y_raw = self.gyro_y_raw * 0.001065
+            gyro_z_raw = self.gyro_z_raw * 0.001065
+        
+            gyro_raw_msg.vector.x = gyro_x_raw
+            gyro_raw_msg.vector.y = gyro_y_raw
+            gyro_raw_msg.vector.z = gyro_z_raw
+            self.pub_raw_gyro.publish(gyro_raw_msg)
+        
+        if self.gyro_LOG and self.gyro_flag:
+            gyro_msg = Vector3Stamped()
+            gyro_msg.header.stamp = rospy.Time.now()
+            
+            # deg/s to rad/s
+            gyro_x = self.gyro_x * 0.0174533
+            gyro_y = self.gyro_y * 0.0174533
+            gyro_z = self.gyro_z * 0.0174533
+            
+            gyro_msg.vector.x = gyro_x
+            gyro_msg.vector.y = gyro_y
+            gyro_msg.vector.z = gyro_z
 
-    def publish_twist(self, msg):        
-        roll = msg.linear.y
-        pitch = msg.linear.x
-        thrust = msg.linear.z
-        yawrate = msg.angular.z
-        self._cf.commander.send_setpoint(-roll, pitch, -yawrate, int(thrust))
+            self.pub_gyro.publish(gyro_msg)
+
+    def publish_twist(self, msg):
+        
+        if self.use_body_rate:
+            rollrate = msg.angular.x
+            pitchrate = msg.angular.y
+            yawrate = msg.angular.z
+            thrust = msg.linear.z
+            
+            # Rad to deg
+            rollrate = rollrate * 57.2958
+            pitchrate = pitchrate * 57.2958
+            yawrate = yawrate * 57.2958
+            
+            self._cf.commander.send_setpoint(rollrate, pitchrate, -yawrate, int(thrust))
+        else:
+            roll = msg.angular.x
+            pitch = msg.angular.y
+            yawrate = msg.angular.z
+            thrust = msg.linear.z
+            
+            # Rad to deg
+            roll = roll * 57.2958
+            pitch = pitch * 57.2958
+            yawrate = yawrate * 57.2958
+            
+            self._cf.commander.send_setpoint(-roll, pitch, -yawrate, int(thrust))
 
 if __name__ == '__main__':
     node = CrazyflieServerNode()
